9792c15ab8d7c734eeacf9b8681a5a6679167b89
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-10-14 09:34:06 +0530

    Ad: json data for periodic table elements - json file as js module using import method

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 import Header from '../Header'
 import Footer from '../Footer';
+import ElementsData from '../../assets/PeriodicTable'
 const Home = () => {
+  console.log('rendering...')
   return (
       <div>
         <Header/>
