1fe2d82e2aad331b1a2191ee76095cd31123b29f
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-10-15 00:46:57 +0530

    formatting

--- src/js/components/Element.js
+++ src/js/components/Element.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from "react";
 
-const Element = ({ index, className, value }) => {
-  return (
-    <div key={index} className={className}>
-      {value.Symbol}
-    </div>
-  );
-};
+const Element = ({ index, className, value }) => (
+  <div key={index} className={className}>
+    {value.Symbol}
+  </div>
+);
 
 export default Element;
