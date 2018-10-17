4d963cf243470ce59553708484fd5b0e897507c7
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-10-17 17:34:09 +0530

    Add: css color for element based on group, Add: css fpr desc and totle box

--- src/js/components/Element.js
+++ src/js/components/Element.js
@@ -1,9 +1,131 @@
 import React, { Fragment } from "react";
+import Styles from "../../css/Element.css";
 
-const Element = ({ index, className, value }) => (
-  <div key={index} className={className}>
-    {value.Symbol}
-  </div>
-);
+let getELementSpecificProperties = (elementId, groupId, className) => {
+  let obj = {};
+
+  switch (groupId) {
+    case 1:
+      if (elementId == 1) {
+        obj = {
+          cell: className.concat(" ", Styles.element),
+          name: Styles.elementName.concat(" ", Styles.hydrogenName),
+          number: Styles.elementNo.concat(" ", Styles.hydrogenNumber)
+        };
+      } else {
+        obj = {
+          cell: className.concat(" ", Styles.element),
+          name: Styles.elementName.concat(" ", Styles.groupOneName),
+          number: Styles.elementNo.concat(" ", Styles.groupOneNumber)
+        };
+      }
+      break;
+    case 2:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupTwoName),
+        number: Styles.elementNo.concat(" ", Styles.groupTwoNumber)
+      };
+      break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupThreeToTwelveName),
+        number: Styles.elementNo.concat(" ", Styles.groupThreeToTwelveNumber)
+      };
+      break;
+    case 13:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupThirteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupThirteenNumber)
+      };
+      break;
+    case 14:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupFourteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupFourteenNumber)
+      };
+      break;
+    case 15:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupFifteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupFifteenNumber)
+      };
+      break;
+    case 16:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupSixteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupSixteenNumber)
+      };
+      break;
+    case 17:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupSeventeenName),
+        number: Styles.elementNo.concat(" ", Styles.groupSeventeenNumber)
+      };
+      break;
+    case 18:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupEighteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupEighteenNumber)
+      };
+      break;
+    case 19:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupNineteenName),
+        number: Styles.elementNo.concat(" ", Styles.groupNineteenNumber)
+      };
+      break;
+    case 20:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName.concat(" ", Styles.groupTwentyName),
+        number: Styles.elementNo.concat(" ", Styles.groupTwentyNumber)
+      };
+      break;
+    default:
+      obj = {
+        cell: className.concat(" ", Styles.element),
+        name: Styles.elementName,
+        number: Styles.elementNo
+      };
+      break;
+  }
+  return obj;
+};
+
+const onHover = () => {
+  console.log('hover')
+}
+
+const Element = ({ index, className, value }) => {
+  let stylesObj = getELementSpecificProperties(
+    value.ElementID,
+    value.GroupID,
+    className
+  );
+  return (
+    <div key={index} className={stylesObj.cell} onMouseOver={onHover}>
+      <div className={stylesObj.name}>{value.Symbol}</div>
+      <div className={stylesObj.number}>{value.ElementID}</div>
+    </div>
+  );
+};
 
 export default Element;
--- src/js/components/PeriodicTable.js
+++ src/js/components/PeriodicTable.js
@@ -4,120 +4,160 @@ import ElementsData from "../../assets/PeriodicTable";
 import Styles from "../../css/PeriodicTable.css";
 import Element from "./Element";
 
-const PeriodicTable = () => {
-  console.log("rendering...");
-  console.log(Styles);
-  const firstRow = ElementsData.Elements.slice(0, 2);
-  const secondAndThirdRow = ElementsData.Elements.slice(2, 18);
-  const FourthAndFifthRow = ElementsData.Elements.slice(18, 54);
-  const SixthRow = [].concat(
-    ElementsData.Elements.slice(54, 57),
-    ElementsData.Elements.slice(71, 86)
-  );
-  const SeventhRow = [].concat(
-    ElementsData.Elements.slice(86, 89),
-    ElementsData.Elements.slice(103, 118)
-  );
-  const EigthRow = ElementsData.Elements.slice(57, 71);
-  const NinthRow = ElementsData.Elements.slice(89, 103);
-
-  // This is a design pattern for React - Layout Component
-  // This must be a Class Component because we need shouldComponentUpdate
-  // with returning value to be false always to prevent unnecessary rendering
-  // This could be either a Stateless or Statefull Class Component
-  class Container extends React.Component {
-    shouldComponentUpdate() {
-      return false;
-    }
-
-    render() {
-      return <div className={Styles.container}>{this.props.children}</div>;
-    }
+// This is a design pattern for React - Layout Component
+// This must be a Class Component because we need shouldComponentUpdate
+// with returning value to be false always to prevent unnecessary rendering
+// This could be either a Stateless or Statefull Class Component
+class Container extends React.Component {
+  shouldComponentUpdate() {
+    return false;
   }
 
-  // This is a design pattern for React - Layout Component
-  // This must be a Class Component because we need shouldComponentUpdate
-  // with returning value to be false always to prevent unnecessary rendering
-  // This could be either a Stateless or Statefull Class Component
-  class GridWrapper extends React.Component {
-    shouldComponentUpdate() {
-      return false;
-    }
+  render() {
+    return <div className={Styles.container}>{this.props.children}</div>;
+  }
+}
 
-    render() {
-      return <div className={Styles.wrapper}>{this.props.children}</div>;
-    }
+// This is a design pattern for React - Layout Component
+// This must be a Class Component because we need shouldComponentUpdate
+// with returning value to be false always to prevent unnecessary rendering
+// This could be either a Stateless or Statefull Class Component
+class GridWrapper extends React.Component {
+  shouldComponentUpdate() {
+    return false;
   }
 
-  // This is a Design pattern for React - Container Component
-  // It is a View only Container.
-  // Other name for this is - Presentational Component
-  // It is a Stateless Dumb Functional Component - Not a Class
-  // We can also make it a Class Component if we need to use
-  // Lifecycle methods to manage certain things, then it would be
-  // a Stateless Class Component or StateFull Class Component
-  // A Statefull Class Component is actually a Layout Component and
-  // A Stateless Functional Component is a Container Component
-  const ElementList = ({ row, checkAt = null, secondaryClass = "" }) => (
-    <Fragment>
-      {row.map((v, key) => (
-        <Element
-          key={key}
-          index={key}
-          className={
-            key === checkAt
-              ? Styles.cell.concat(" ", secondaryClass)
-              : Styles.cell
-          }
-          value={v}
-        />
-      ))}
-    </Fragment>
-  );
+  render() {
+    return <div className={Styles.wrapper}>{this.props.children}</div>;
+  }
+}
 
-  // Below are all Stateless Dumb Functional Components
-  // or Presentational Components
-  // or View Only Components
-  const Header = () => <div className={Styles.header} />;
-  const Description = () => <div className={Styles.description} />;
-  const Controls = () => <div className={Styles.controls}>controls</div>;
+// This is a Design pattern for React - Container Component
+// It is a View only Container.
+// Other name for this is - Presentational Component
+// It is a Stateless Dumb Functional Component - Not a Class
+// We can also make it a Class Component if we need to use
+// Lifecycle methods to manage certain things, then it would be
+// a Stateless Class Component or StateFull Class Component
+// A Statefull Class Component is actually a Layout Component and
+// A Stateless Functional Component is a Container Component
+const ElementList = ({ row, checkAt = null, secondaryClass = "" }) => (
+  <Fragment>
+    {row.map((v, key) => (
+      <Element
+        key={key}
+        index={key}
+        className={
+          key === checkAt
+            ? Styles.cell.concat(" ", secondaryClass)
+            : Styles.cell
+        }
+        value={v}
+      />
+    ))}
+  </Fragment>
+);
 
+// Below are all Stateless Dumb Functional Components
+// or Presentational Components
+// or View Only Components
+const Header = () => <div className={Styles.header}>{"Periodic Table"}</div>;
+const desc =
+  "The Royal Society of Chemistry's interactive periodic table features history, alchemy, podcasts, videos, and data trends across the periodic table. Click the tabs at the top to explore each section. Use the buttons above to change your view of the periodic table and view Murray Robertson’s stunning Visual Elements artwork. Click each element to read detailed information.";
+const Description = () => <div className={Styles.description}>{desc}</div>;
+const Controls = () => <div className={Styles.controls}>controls</div>;
+const ElementDescription = () => {
   return (
-    <Container>
-      <Controls />
-      <GridWrapper>
-        <Header />
-        <ElementList row={firstRow} checkAt={1} secondaryClass={Styles.He} />
-      </GridWrapper>
-      <GridWrapper>
-        <Description />
-        <ElementList row={secondAndThirdRow} className={Styles.cell} />
-      </GridWrapper>
-      <GridWrapper>
-        <ElementList row={FourthAndFifthRow} className={Styles.cell} />
-      </GridWrapper>
-      <GridWrapper>
-        <ElementList row={SixthRow} className={Styles.cell} />
-      </GridWrapper>
-      <GridWrapper>
-        <ElementList row={SeventhRow} className={Styles.cell} />
-      </GridWrapper>
-      <GridWrapper>
-        <ElementList
-          row={EigthRow}
-          checkAt={0}
-          secondaryClass={Styles.fBlock}
-        />
-      </GridWrapper>
-      <GridWrapper>
-        <ElementList
-          row={NinthRow}
-          checkAt={0}
-          secondaryClass={Styles.fBlock}
-        />
-      </GridWrapper>
-    </Container>
+    <div className={Styles.elementDescription}>
+      <div>
+        <img  className={Styles.elementImg} src="http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/Images/Elements/Cerium-S.png?6.2.0.0" />
+      </div>
+      <div className={Styles.elementDescriptionTable}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+      <div className={Styles.elementDescriptionTable}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+      <div className={Styles.bigCell}>
+        <div className={Styles.bigCellLabel}>
+          <div className={Styles.bigCellLabelIconBigText}>
+            Al
+          </div>
+          <div>Aluminium</div>
+        </div>
+        <div className={Styles.bigCellDescription}>
+          <div>102</div>
+          <div>3.23</div>
+        </div>
+      </div>
+    </div>
   );
 };
+// const PeriodicTable = () => {
+class PeriodicTable extends React.PureComponent {
+  constructor() {
+    super();
+  }
+  render() {
+    console.log("rendering...");
+    console.log(Styles);
+    const firstRow = ElementsData.Elements.slice(0, 2);
+    const secondAndThirdRow = ElementsData.Elements.slice(2, 18);
+    const FourthAndFifthRow = ElementsData.Elements.slice(18, 54);
+    const SixthRow = [].concat(
+      ElementsData.Elements.slice(54, 57),
+      ElementsData.Elements.slice(71, 86)
+    );
+    const SeventhRow = [].concat(
+      ElementsData.Elements.slice(86, 89),
+      ElementsData.Elements.slice(103, 118)
+    );
+    const EigthRow = ElementsData.Elements.slice(57, 71);
+    const NinthRow = ElementsData.Elements.slice(89, 103);
+
+    return (
+      <Container>
+        <Controls />
+        <GridWrapper>
+          <Header />
+          <ElementList row={firstRow} checkAt={1} secondaryClass={Styles.He} />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementDescription />
+          <ElementList row={secondAndThirdRow} className={Styles.cell} />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementList row={FourthAndFifthRow} className={Styles.cell} />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementList row={SixthRow} className={Styles.cell} />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementList row={SeventhRow} className={Styles.cell} />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementList
+            row={EigthRow}
+            checkAt={0}
+            secondaryClass={Styles.fBlock}
+          />
+        </GridWrapper>
+        <GridWrapper>
+          <ElementList
+            row={NinthRow}
+            checkAt={0}
+            secondaryClass={Styles.fBlock}
+          />
+        </GridWrapper>
+      </Container>
+    );
+  }
+}
 
 export default PeriodicTable;
