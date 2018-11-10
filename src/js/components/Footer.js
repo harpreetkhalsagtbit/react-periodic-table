5cb79e6437127b83ed00c4ff6d74b16891314089
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-11-10 16:20:57 +0530

    Update: css

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -1,13 +1,176 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "../../css/Footer.css";
 
 const Footer = () => {
   return (
-      <footer>
-        <span>
-        © Royal Society of Chemistry 2018
-        </span>
-      </footer>
+    <footer>
+      <div className={Styles.footerSubMenu}>
+        <ul className={Styles.breadCumbs}>
+          <li>
+            <Link to="/">rsc.org</Link>
+          </li>
+          <li>
+            <img
+              src="//www.rsc-cdn.org/global/header-footer/images/icons/chevron-right-light.png"
+              width="8"
+              height="8"
+              alt=""
+            />
+          </li>
+          <li>
+            <Link to="/">Periodic Table</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={Styles.footerMenu}>
+        <div className={Styles.container}>
+          <div className={Styles.logoWrapper}>
+            <img
+              src="//www.rsc-cdn.org/global/header-footer/images/rsc-logo-ftr.png"
+              width="180"
+              height="120"
+              alt=""
+            />
+          </div>
+          <div>
+            <span>Explore</span>
+            <ul className={Styles.listWrapper}>
+              <li>
+                <Link to="//www.rsc.org/">Home</Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/about-us/">About us</Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/campaigning-outreach/">
+                  Campaigning &amp; outreach
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/journals-books-databases/">
+                  Journals, books &amp; databases
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/news-events/">
+                  News&nbsp;&amp; events
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/locations-contacts/">
+                  Locations &amp; contacts
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/awards-funding/">
+                  Awards &amp; funding
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/advertise/">Advertise</Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/help-legal/">Privacy policy</Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/help-legal/">
+                  Terms &amp; conditions
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span>Membership</span>
+            <ul className={Styles.listWrapperTwo}>
+              <li>
+                <Link to="//www.rsc.org/membership-and-community/">
+                  Membership &amp; professional community
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/resources-tools/">
+                  Resources &amp; tools
+                </Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/careers/">Careers</Link>
+              </li>
+              <li>
+                <Link to="//www.rsc.org/help-legal/">Help &amp; legal</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={Styles.containerBottom}>
+          <div>
+            <ul>
+              <li>© Royal Society of Chemistry 2018</li>
+              <li>Registered charity number: 207890</li>
+            </ul>
+          </div>
+          <div>
+            <dt className={Styles.socialIcons}>
+              <dl className={Styles.facebook}>
+                <Link to="https://www.facebook.com/RoyalSocietyofChemistry">
+                  <span>Facebook</span>
+                  <img
+                    alt="facebook"
+                    src="https://www.rsc-cdn.org/global/header-footer/images/icons/facebook.png"
+                    height="32"
+                    width="32"
+                  />
+                </Link>
+              </dl>
+              <dl className={Styles.twitter}>
+                <Link to="https://twitter.com/RoySocChem">
+                  <span>Twitter</span>
+                  <img
+                    alt="twitter"
+                    src="https://www.rsc-cdn.org/global/header-footer/images/icons/twitter.png"
+                    height="32"
+                    width="32"
+                  />
+                </Link>
+              </dl>
+              <dl className={Styles.linkedin}>
+                <Link to="https://www.linkedin.com/company/23105">
+                  <span>LinkedIn</span>
+                  <img
+                    alt="linkedin"
+                    src="https://www.rsc-cdn.org/global/header-footer/images/icons/linkedin.png"
+                    height="32"
+                    width="32"
+                  />
+                </Link>
+              </dl>
+              <dl className={Styles.youtube}>
+                <Link to="https://www.youtube.com/user/wwwRSCorg">
+                  <span>Youtube</span>
+                  <img
+                    alt="youtube"
+                    src="https://www.rsc-cdn.org/global/header-footer/images/icons/youtube.png"
+                    height="32"
+                    width="32"
+                  />
+                </Link>
+              </dl>
+              <dl className={Styles.googleplus}>
+                <Link to="https://plus.google.com/113399675911040898889">
+                  <span>Google Plus</span>
+                  <img
+                    alt="googleplus"
+                    src="https://www.rsc-cdn.org/global/header-footer/images/icons/googleplus.png"
+                    height="32"
+                    width="32"
+                  />
+                </Link>
+              </dl>
+            </dt>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- src/js/pages/Home/index.js
+++ src/js/pages/Home/index.js
@@ -5,12 +5,12 @@ import Footer from "../../components/Footer";
 import PeriodicTable from "../../components/PeriodicTable";
 import Styles from "../../../css/Home.css";
 
-class Home extends React.PureComponent  {
+class Home extends React.PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isHoverActive:false
-    }
+      isHoverActive: false
+    };
   }
 
   render() {
@@ -19,14 +19,55 @@ class Home extends React.PureComponent  {
         <Header />
         <main onMouseOver={this.onMouseOver}>
           <div className={Styles.homePageLogo}>
-            <img src="http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/images/periodic-table-logo.png" alt="Royal Society of Chemistry - Periodic table"></img>
+            <img
+              src="http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/images/periodic-table-logo.png"
+              alt="Royal Society of Chemistry - Periodic table"
+            />
           </div>
-          <PeriodicTable/>
+          <PeriodicTable />
+          <div className={Styles.container}>
+            <span className={Styles.appDownloadText}>
+              Download our free Periodic Table app for mobile phones and
+              tablets.
+            </span>
+            <span>
+              <a href="https://itunes.apple.com/app/periodic-table/id1019468967?ls=1&amp;mt=8">
+                <img
+                  alt="App store"
+                  title="App store"
+                  src="http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/Images/app_store_img.png?6.2.0.0"
+                />
+              </a>
+            </span>
+            <span>
+              <a href="https://play.google.com/store/apps/details?id=org.rsc.periodictable&amp;hl=en_GB">
+                <img
+                  alt="Google play"
+                  title="Google play"
+                  src="http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/Images/google_play_img.png?6.2.0.0"
+                />
+              </a>
+            </span>
+          </div>
+          <div className={Styles.socialbarContainer}>
+            <a
+              class="social_newsletter_image social_icons_newsletter newsletterclick"
+              href="http://www.rsc.org/learn-chemistry/newsletter"
+            >
+              &nbsp;
+            </a>
+            <a
+              class="social_feedback_image social_icons_feedback feedbackclick"
+              href="http://www.rsc.org/learn-chemistry/feedback"
+            >
+              &nbsp;
+            </a>
+          </div>
         </main>
         <Footer />
       </Fragment>
     );
   }
-};
+}
 
 export default Home;
